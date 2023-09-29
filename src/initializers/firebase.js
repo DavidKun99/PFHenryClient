b80390640494a51cf1b44ddb1f29ee5b9c77c2ec
David Acevedo <50499045+DavidKun99@users.noreply.github.com>
2023-09-28 21:04:19 -0500

    Actualizar firebase.js

--- src/initializers/firebase.js
+++ src/initializers/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvW7Rpj1Mj0CB9jW1MtXyFQj9nbdqG23E",
-  authDomain: "pfhenryg7-45229.firebaseapp.com",
-  projectId: "pfhenryg7-45229",
-  storageBucket: "pfhenryg7-45229.appspot.com",
-  messagingSenderId: "576841377648",
-  appId: "1:576841377648:web:83d0ba1685ff6cfe59d1b5",
-  measurementId: "G-ELVDMMK69M"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO.firebaseapp.com",
+  projectId: "TU_PROYECTO_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID",
+  measurementId: "TU_MEASUREMENT_ID",
 };
 
-const firebase = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+// Configura la persistencia de sesión como "NONE"
+firebaseApp.auth().setPersistence(firebaseApp.auth.Auth.Persistence.NONE);
 
-export default firebase
+export default firebaseApp;
